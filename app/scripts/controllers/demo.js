3c1a2cb29a7b009e18078dfb7f97fc2211955d40
paulbarrett79 <paulbarrett79@gmail.com>
2014-12-03 16:20:31 +0000

    Beginning integrating external service for chem property lookup

--- app/scripts/controllers/demo.js
+++ app/scripts/controllers/demo.js
@@ -9,7 +9,7 @@
  */
 var app = angular.module('ngChemApp');
 
-app.controller('DemoCtrl', function ($scope, $rootScope, $state) {
+app.controller('DemoCtrl', function ($scope, $rootScope, $state, ChEMBLFactory) {
 
 	$scope.formData = {};
 
@@ -20,10 +20,9 @@ app.controller('DemoCtrl', function ($scope, $rootScope, $state) {
     $rootScope.dynamic = 0;
     $rootScope.max = 100;
 
-	// function to process the form
-	$scope.processForm = function () {
-	  alert('awesome!');
-	};
+	$scope.alerts = [
+        /*{ type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' }*/
+	];
 
 	$scope.parsed_input = [];
 
@@ -31,7 +30,8 @@ app.controller('DemoCtrl', function ($scope, $rootScope, $state) {
 
 	$scope.gridOptions = { data: 'parsed_input',
 						   columnDefs: [{field: 'type', displayName: 'Type'},
-                                        {field:'input_str', displayName:'Value'}]
+                                        {field:'input_str', displayName:'Value'},
+                                        {field:'extraData', displayName:'From Factory'}]
 	                      };
 
 	$scope.parseInput = function (){
@@ -45,18 +45,54 @@ app.controller('DemoCtrl', function ($scope, $rootScope, $state) {
 		var splitted = splitInput(this.input_string);
 
 		jQuery.each(splitted, function(idx,val) {
+			
+			//intervene here with Chembl calls using the ChEMBLFactory
+        	//interrogate each val for its type, then call the appropriate ChEMBL service to retrieve data
+        	var returned_json = {}
+        	if (val.type == 'InChi') {
+        		//val.extraData = ChEMBLFactory.queryInChi();
+        		ChEMBLFactory.queryInChi().then(function(response){
+		        	$scope.addAlert("success", response.message );
+        		},function(error) {
+        			$scope.addAlert("danger", response.message );
+        		});
+        	}
+        	else if (val.type == 'InChi Key') {
+        		//val.extraData = ChEMBLFactory.queryInChiKey();
+        		var returned_json = ChEMBLFactory.queryInChiKey().then(function(response){
+		        	$scope.addAlert("success", response.message );
+        		},function(error) {
+        			$scope.addAlert("danger", response.message );
+        		});
+        	}
+        	else if (val.type == 'SMILES') {
+        		//val.extraData = ChEMBLFactory.querySmiles("");
+        		var returned_json = ChEMBLFactory.querySmiles("").then(function(response){
+		        	$scope.addAlert("success", response.message );
+        		},function(error) {
+        			$scope.addAlert("danger", response.message );
+        		});
+        	}
+        	else if (val.type == 'ChEMBL ID') {
+        		//val.extraData = ChEMBLFactory.queryChemblId();
+        		var returned_json = ChEMBLFactory.queryChemblId().then(function(response){
+		        	$scope.addAlert("success", response.message );
+        		},function(error) {
+        			$scope.addAlert("danger", response.message );
+        		});
+        	}
+
+        	//display errors
+        	if (returned_json.resp == "danger") {
+        		$scope.addAlert(returned_json.resp, returned_json.message );
+        	}
 			$scope.parsed_input.push(val);	
 		});
+
+
+
 		$scope.parsed_input.push(splitted);
-	}
-
-
-});
-
-app.controller('MessageCtrl', function ($scope){
-	$scope.alerts = [
-        /*{ type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' }*/
-	];
+	};
 
     $scope.addAlert = function(message, alert_type){
       $scope.alerts.push({msg: message || 'There has been an error!', type : alert_type || "danger" });
@@ -66,4 +102,14 @@ app.controller('MessageCtrl', function ($scope){
       $scope.alerts.splice(index, 1);
     };
 
+
+
+
 });
+
+app.controller('MessageCtrl', function ($scope){
+	
+
+});
+
+
