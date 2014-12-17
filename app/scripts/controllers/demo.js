55a5d5ccd4150189b1d01cf7a02a8c892b42a809
paulbarrett79 <paulbarrett79@gmail.com>
2014-12-17 18:07:30 +0000

    removing default data and changing 2 decimal place numbers

--- app/scripts/controllers/demo.js
+++ app/scripts/controllers/demo.js
@@ -31,9 +31,8 @@ app.controller('DemoCtrl', function ($scope, $rootScope, $state, ChEMBLFactory, 
     $scope.validated = { "warnings": {
                             "inorganicCount": "0",
                             "painsCount": "0",
-                            "painsQuinone_A": "{\"smarts\": \"[!#6&!#1]=[#6]-1-[#6]=,:[#6]-[#6](=[!#6&!#1])-[#6]=,:[#6]-1\"}",
                             "saltCount": "0",
-                            "tautomerCount": "2"
+                            "tautomerCount": "0"
                           }
                         };
 
