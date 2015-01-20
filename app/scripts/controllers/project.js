18cca61359e49339c45a4675b113e96bd42752b3
paulbarrett79 <paulbarrett79@gmail.com>
2015-01-20 11:01:34 +0000

    Adding barebones project controller

---
+++ app/scripts/controllers/project.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngChemApp.controller:ProjectctrlCtrl
+ * @description
+ * # ProjectctrlCtrl
+ * Controller of the ngChemApp
+ */
+angular.module('ngChemApp')
+  .controller('ProjectctrlCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
