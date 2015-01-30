31d85347fd53f5d4d5328cd81f3222c77bad9c27
Strets123 <strets123@gmail.com>
2015-01-30 08:01:12 +0000

    fix links and project data

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -93,20 +93,20 @@ angular.module('ngChemApp', [
 
                 $scope.errors = [];
 
-                $scope.stereochem_options = [{ name:'1', value:'as drawn'},
-                                            { name:'2', value:'achiral',}, 
-                                            { name:'3', value:'racemic',}, 
-                                            { name:'4', value:'single enantiomer of known absolute configuration'},
-                                            { name:'5', value:'single enantiomer of unknown absolute configuration'},
-                                            { name:'6', value:'single diastereoisomer of known absolute configuration'},
-                                            { name:'7', value:'single diastereoisomer of unknown absolute configuration'},
-                                            { name:'8', value:'meso'},
-                                            { name:'9', value:'E'},
-                                            { name:'10', value:'Z'},
-                                            { name:'11', value:'mixture E/Z'},
-                                            { name:'12', value:'cis'},
-                                            { name:'13', value:'trans'},
-                                            { name:'14', value:'mixture of diastereoisomers'}
+                $scope.stereochem_options = [{ name:'-1', value:'as drawn'},
+                                            { name:'6', value:'achiral',}, 
+                                            { name:'7', value:'racemic',}, 
+                                            { name:'8', value:'single enantiomer of known absolute configuration'},
+                                            { name:'9', value:'single enantiomer of unknown absolute configuration'},
+                                            { name:'10', value:'single diastereoisomer of known absolute configuration'},
+                                            { name:'11', value:'single diastereoisomer of unknown absolute configuration'},
+                                            { name:'12', value:'meso'},
+                                            { name:'13', value:'E'},
+                                            { name:'14', value:'Z'},
+                                            { name:'15', value:'mixture E/Z'},
+                                            { name:'16', value:'cis'},
+                                            { name:'17', value:'trans'},
+                                            { name:'18', value:'mixture of diastereoisomers'}
                                             ];
                 //default selection set in the scope molecule object, select box value bound to form.                                           
                 
