3df2487d3f50c29f2f5e0033104c8bfe21a84f8a
chembiohub <you@example.com>
2015-03-02 15:34:45 +0000

    some gruntfile changes to ensure that image file references are updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -244,7 +244,7 @@ module.exports = function (grunt) {
 
     // Performs rewrites based on filerev and the useminPrepare configuration
     usemin: {
-      html: ['<%= yeoman.dist %>/{,*/}*.html'],
+      html: ['<%= yeoman.dist %>/{,*/}*.html','<%= yeoman.dist %>/views/{,*/}*.html', '<%= yeoman.dist %>/views/templates/{,*/}*.html' ],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
       options: {
         assetsDirs: ['<%= yeoman.dist %>','<%= yeoman.dist %>/images']
