ef5e1e7e717712b5541fc50a39bf275d6681d00b
Strets123 <strets123@gmail.com>
2015-03-03 17:12:15 +0000

    config

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -8,7 +8,14 @@ if (arr[2] != 'localhost:9000'){
 
 var path = arr[0] + "//" + arr[2] + "/" + part +"/";
 
-var configuration = {"cbh_batch_upload": {"list_endpoint": path + "cbh_batch_upload", "schema": path + "cbh_batch_upload/schema"}, "cbh_projects": {"list_endpoint": path + "cbh_projects", "schema": path + "cbh_projects/schema"}, "cbh_compound_batches": {"list_endpoint": path + "cbh_compound_batches", "schema": path + "cbh_compound_batches/schema"}, "users": {"list_endpoint": path + "users", "schema": path + "users/schema"}}
+var configuration = {"cbh_batch_upload": 
+    {"list_endpoint": path + "cbh_batch_upload", 
+    "schema": path + "cbh_batch_upload/schema"}, 
+    "cbh_projects": {"list_endpoint": path + 
+    "cbh_projects", "schema": path + "cbh_projects/schema"}, 
+    "cbh_compound_batches": {"list_endpoint": path + 
+    "cbh_compound_batches", "schema": path + "cbh_compound_batches/schema"}, 
+    "users": {"list_endpoint": path + "users", "schema": path + "users/schema"}};
 
 angular.module('ngChemApp').value('urlConfig',  
   configuration
