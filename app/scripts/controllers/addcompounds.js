6159db9262bcde58175b8f4f6bdb8eb9171b0355
Strets123 <strets123@gmail.com>
2015-06-05 13:07:05 +0100

    beginning work on the new add compounds controller

---
+++ app/scripts/controllers/addcompounds.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngChemApp.controller:AddcompoundsCtrl
+ * @description
+ * # AddcompoundsCtrl
+ * Controller of the ngChemApp
+ */
+angular.module('ngChemApp')
+  .controller('AddcompoundsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
