0c96ea5f904fb9c21d58470f71446a0e3fe0c256
paulbarrett79 <paulbarrett79@gmail.com>
2015-08-26 08:53:08 +0100

    Feedback on front end post-sprint review

--- app/scripts/controllers/dataoverview.js
+++ app/scripts/controllers/dataoverview.js
@@ -8,8 +8,35 @@
  * Controller of the ngChemApp
  */
 angular.module('chembiohubAssayApp')
-  .controller('DataOverviewCtrl', ['$scope', 'AddDataFactory', function ($scope, AddDataFactory) {
-var dataoverviewctrl = this;
+  .controller('DataOverviewCtrl', ['$scope', 'AddDataFactory', '$modal', function ($scope, AddDataFactory, $modal) {
+	var dataoverviewctrl = this;
+
+	$scope.modalInstance = {};
+    $scope.popup_data = {}; 
+
+    dataoverviewctrl.openDetail = function() {
+      
+      $scope.modalInstance = $modal.open({
+        templateUrl: 'views/modal-template.html',
+        size: 'md',
+        resolve: {
+          popup_data: function () {
+            return $scope.popup_data;
+          },
+
+        }, 
+        controller: function($scope, $modalInstance, popup_data, $timeout) {
+          $scope.popup_data = popup_data;
+          
+          $scope.modalInstance = $modalInstance;
+
+		  $scope.cancel = function () {
+		    $modalInstance.dismiss('cancel');
+		  };
+
+        }
+      });
+    };
 
 
 
