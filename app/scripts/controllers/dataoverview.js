5ee8bc8879a1f17981b3686c4570b3603eab81db
Strets123 <strets123@gmail.com>
2015-09-07 15:58:02 +0100

    tweaks to adding and editing

--- app/scripts/controllers/dataoverview.js
+++ app/scripts/controllers/dataoverview.js
@@ -8,7 +8,8 @@
  * Controller of the ngChemApp
  */
 angular.module('chembiohubAssayApp')
-  .controller('DataOverviewCtrl', ['$scope', 'AddDataFactory', '$modal', '$resource', '$stateParams' , function ($scope, AddDataFactory, $modal, $resource, $stateParams) {
+  .controller('DataOverviewCtrl', ['$scope', 'AddDataFactory', '$modal', '$resource', '$stateParams', '$state' , 
+    function ($scope, AddDataFactory, $modal, $resource, $stateParams, $state) {
 	var dataoverviewctrl = this;
   var classes = {
     'l1': "l1",
@@ -17,32 +18,79 @@ angular.module('chembiohubAssayApp')
 	$scope.modalInstance = {};
     $scope.popup_data = {};
     $scope.getAnnotations = function(dpc){
-        if( angular.isDefined(dpc.id)){
+
           dpc.dfc_full = $scope.assayctrl.dfc_lookup[dpc.data_form_config];
-        dpc.main_cfc = dpc.dfc_full[dpc.level_from];
-        dpc.main_data = dpc[dpc.level_from];
-        dpc.htmlClassName = classes[dpc.level_from];
-        // if( dpc.dfc_full[dpc.level_from] != -1 && dpc.level_from != dpc.dfc_full.last_level){
-        //   dpc.requiresAddChildren = true;
-        // }else{
-        //   dpc.requiresAddChildren = false;
-        // }
-        //here we assume bottom 2 levels will have the same form
-        dpc.next_level_cfc = dpc.dfc_full[dpc.next_level];
-        if(  dpc.next_level == dpc.dfc_full.last_level){
-          dpc.childrenTemplate = "views/templates/overview-children-table.html";
-        }else{
-          dpc.childrenTemplate = "views/templates/overview-children.html";
-        }
-        }
+          dpc.main_cfc = dpc.dfc_full[dpc.level_from];
+          dpc.main_data = dpc[dpc.level_from];
+          dpc.htmlClassName = classes[dpc.level_from];
+        
+          dpc.next_level_cfc = dpc.dfc_full[dpc.next_level];
+          dpc.addingChild = false;
+          dpc.default_data = {'project_data': {} ,'custom_field_config' : dpc.next_level_cfc.resource_uri};
+
+          dpc.setForm = function(defaults){
+            dpc.new_next_level_model = defaults;
+            dpc.next_level_edit_form = [];
+            dpc.next_level_edit_schema = { "type": "object", 'properties' : {}, 'required': [] };
+            angular.forEach(dpc.next_level_cfc.project_data_fields, function(proj_data){
+                  //pull out the edit_form.form and edit_schema.schema
+                  var form = angular.copy(proj_data.edit_form.form[0]);
+                  form.htmlClass = "col-xs-3";
+                  dpc.next_level_edit_form.push(form);
+                  angular.extend(dpc.next_level_edit_schema.properties, angular.copy(proj_data.edit_schema.properties));
+                });
+            dpc.addingChild = false;
+          }
+          dpc.setForm(dpc.default_data);
+
+          dpc.addChild = function(){
+              var AddDF = AddDataFactory.dataClassification;
+           
+              var adfresult = AddDF.get({'dc': dpc.id});
+                  adfresult.$promise.then(function(clone){
+
+                        clone.children = [];
+                        clone.id = null;
+                        clone.resource_uri = null;
+                        clone[dpc.next_level] = dpc.new_next_level_model;
+                        clone.$save(function(data){
+                            $state.go($state.current, $stateParams, {reload: true});
+                        });
+                  });
+          };
+
+          dpc.updateChild = function(child_dpc_id){
+              var AddDF = AddDataFactory.dataClassification;
+              var adfresult = AddDF.get({'dc': child_dpc_id});
+                  adfresult.$promise.then(function(clone){
+                        clone[dpc.next_level] = dpc.new_next_level_model;
+                        clone.$update({'dc': child_dpc_id} ,function(data){
+                            $state.go($state.current, $stateParams, {reload: true});
+                        });
+                  });
+          }
+
+          
+
+          if(  dpc.next_level == dpc.dfc_full.last_level){
+            dpc.childrenTemplate = "views/templates/overview-children-table.html";
+          }else{
+            dpc.childrenTemplate = "views/templates/overview-children.html";
+          }
+        
         
     };
     $scope.iterate_children = function(obj){
         angular.forEach(obj.children, function(child){
-              $scope.getAnnotations(child);
-              $scope.iterate_children(child);
+          if( angular.isDefined(child.id)){
+            child.parentObj = obj;
+            
+            $scope.getAnnotations(child);
+            $scope.iterate_children(child);
+            }
           });
     }
+
     dataoverviewctrl.fetchData = function(){
        AddDataFactory.nestedDataClassification.get({
         "l0_permitteded_projects__project_key": $stateParams.projectKey, 
