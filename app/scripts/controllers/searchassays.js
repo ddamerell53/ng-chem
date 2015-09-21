21316e6cc839d59496f5b1658a35bd8a89199fcb
paulbarrett79 <paulbarrett79@gmail.com>
2015-09-21 11:50:09 +0100

    Search Assays

---
+++ app/scripts/controllers/searchassays.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngChemApp.controller:SearchAssaysCtrl
+ * @description
+ * # SearchAssaysCtrl
+ * Controller of the chembiohubAssayApp
+ */
+angular.module('chembiohubAssayApp')
+  .controller('SearchAssaysCtrl', function ($scope) {
+    
+  });
