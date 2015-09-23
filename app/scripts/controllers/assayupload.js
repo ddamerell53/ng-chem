601fbf29c0b4b02c695299001fdd8df4dd6427f9
paulbarrett79 <paulbarrett79@gmail.com>
2015-09-23 17:25:04 +0100

    More upload page

--- app/scripts/controllers/assayupload.js
+++ app/scripts/controllers/assayupload.js
@@ -8,13 +8,14 @@
  * Controller of the chembiohubAssayApp
  */
 angular.module('chembiohubAssayApp')
-  .controller('AssayUploadCtrl', ['$scope', 'prefix', 'urlConfig', function ($scope, prefix, urlConfig) {
+  .controller('AssayUploadCtrl', ['$scope', 'prefix', 'urlConfig', '$cookies', function ($scope, prefix, urlConfig, $cookies) {
 		$scope.inputData = {inputstring : ""};
         $scope.filedata = {};
         $scope.filesUploading = false;
         $scope.dataReady = false;
 		$scope.csrftoken = $cookies[prefix.split("/")[0] + "csrftoken"];
 		$scope.flowinit = {
+			//need to change target to the new WS path provided by Andy
 		    target: urlConfig.instance_path.url_frag + 'flow/upload/',
 		    headers: {
 		        'X-CSRFToken': $scope.csrftoken
