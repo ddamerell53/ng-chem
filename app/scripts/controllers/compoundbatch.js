bc7aacf187fe68348b322b56f1fd8eefa6c9be6f
paulbarrett79 <paulbarrett79@gmail.com>
2015-11-16 09:03:26 +0000

    Changing paging step options

--- app/scripts/controllers/compoundbatch.js
+++ app/scripts/controllers/compoundbatch.js
@@ -29,9 +29,9 @@ angular.module('chembiohubAssayApp')
                 $scope.listOrGallery.choice = $stateParams.viewType;
             }
             var listPerPage = [
-                { label: "10/page", value: "10" },
                 { label: "20/page", value: "20" },
                 { label: "50/page", value: "50" },
+                { label: "100/page", value: "100" },
             ];
 
             var galleryPerPage = {
