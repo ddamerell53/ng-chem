ba41b77de9dff5d2bee026869293a93f5cdbafdf
Strets123 <strets123@gmail.com>
2015-11-24 15:10:11 +0000

    new elasticsearch

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -51,6 +51,7 @@ angular.module('chembiohubAssayApp').value('prefix',
 var initInjector = angular.injector(["ng"]);
 
 var $http = initInjector.get("$http");
+var $q = initInjector.get("$q");
 
 var $timeout = initInjector.get("$timeout");
 
@@ -214,3 +215,45 @@ angular.module('chembiohubAssayApp').constant('euiHost',
    arr[0] + "//" + arr[2] + "/" + part + "/datastore"
 );
 
+angular.module('elasticsearch', [])
+.factory('esFactory', ['$http', '$q', function ($http, $q) {
+    return function (clientOpts) {
+        return {
+            search: function (searchOpts) {
+                var deferredAbort = $q.defer();
+                var defer = $q.defer();
+
+                $http({
+                    method: 'POST',
+                    url: clientOpts.host + "/" + searchOpts.index + '/_search',
+                    params: {
+                        size: searchOpts.size,
+                        from: searchOpts.from
+                    },
+                    data: searchOpts.body.toJSON(),
+                    timeout: deferredAbort.promise
+                }).then(
+                  function (body) {
+                      defer.resolve(body.data);
+                  },
+                  function () {
+                      defer.reject.apply(this, arguments);
+                  }
+                );
+
+                defer.promise.abort = function () {
+                    deferredAbort.resolve();
+                };
+
+                // cleanup (http://stackoverflow.com/questions/24440177/angularjs-how-to-cancel-resource-promise-when-switching-routes)
+                defer.promise.finally(function() {
+                        defer.promise.abort = angular.noop;
+                        deferredAbort = defer = null;
+                    }
+                );
+
+                return defer.promise;
+            }
+        };
+    };
+}]);
