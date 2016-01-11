3561c2d32e6e06a68210aeb0feb9b5b801e8a3d6
Strets123 <strets123@gmail.com>
2016-01-11 10:03:39 +0000

    always rebuild chemdoodle in IE

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,6 +8,35 @@
  *
  * Main module of the application.
  */
+
+
+function detectIE() {
+    var ua = window.navigator.userAgent;
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+       // Edge (IE 12+) => return version number
+       return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
+}
+
+ 
 angular.module('chembiohubAssayApp')
 
 
@@ -32,6 +61,7 @@ $urlRouterProvider.when('', '/projects/list');
         controller: function($scope, $rootScope, $state, $location, $modal, urlConfig, loggedInUser, projectList, prefix, $compile, MessageFactory, skinConfig, InvitationFactory) {
 
           var cbh = this;
+          cbh.isIE = detectIE();
           cbh.appName = "Platform"
           cbh.logged_in_user = loggedInUser;
           cbh.projects = projectList;
