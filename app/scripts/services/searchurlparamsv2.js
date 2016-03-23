03c39ca42ca6a7565873eae49a57d9ecc7a540ee
paulbarrett79 <paulbarrett79@gmail.com>
2016-03-23 15:56:33 +0000

    TODOs for error handling

--- app/scripts/services/searchurlparamsv2.js
+++ app/scripts/services/searchurlparamsv2.js
@@ -205,7 +205,7 @@ angular.module('chembiohubAssayApp')
               backend_query[p] = params[p];
 
             })
-            
+            //TODO hanlde error from this call
             var promise = CBHCompoundBatch.queryv2(backend_query).then(function(result) {
 
               //get the single result, and add this as a parameter to encoded_query
