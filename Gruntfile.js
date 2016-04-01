a2b88880bea6c71f8b4be6f374edd312747cb428
Strets123 <strets123@gmail.com>
2016-04-01 10:11:02 +0100

    add single compound buttons

--- Gruntfile.js
+++ Gruntfile.js
@@ -499,6 +499,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
     grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+
     grunt.task.run(['serve:' + target]);
   });
 
@@ -507,7 +508,8 @@ module.exports = function (grunt) {
     'concurrent:test',
     'autoprefixer',
     'connect:test',
-    'karma'
+    'karma',
+
   ]);
 
   grunt.registerTask('build', [
