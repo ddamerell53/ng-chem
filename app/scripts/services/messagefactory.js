8b399ec2d28f269c80f175f87c223a2d436fc298
paulbarrett79 <paulbarrett79@gmail.com>
2016-04-20 10:54:16 +0100

    Project list changes thesgc/chembiohub_helpdesk#922

--- app/scripts/services/messagefactory.js
+++ app/scripts/services/messagefactory.js
@@ -47,8 +47,8 @@ angular.module('chembiohubAssayApp')
     var messages = {
         //"home": {
             wizard_intro_text: "To upload multiple compounds you will need a list of SMILES; an InChI key or list of InChI keys; or either a SD, ChemDraw or Excel file.",
-            projects_help: "For each project you can view assay, inventory and chemical data dependent on the setup of the project. Editors can edit data, owners are able to invite new users to a project and change the usable fields or nominate other owners.",
-            project_list_filter_help: "Narrow down the project list using this quick search box. Results will filter on project name and project type.",
+            projects_help: "For each project you have various options available dependent on the setup of the project. If you believe you should have access to projects not shown or the functions available for each are not what you expect, contact your administrator",
+            project_list_filter_help: "Narrow down the project list using this quick search box. Results will filter on project name.",
             plate_list_filter_help: "Narrow down the plate list using this quick search box. Results will filter on plate name and description.",
 
         //},
